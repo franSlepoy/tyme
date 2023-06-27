12cbf1d2886344d8cd1714c4e8f0aa74f89b98db
franSlepoy <slepoyfrancisco@gmail.com>
2023-06-27 17:21:42 -0300

    subiendo

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Button} from "@mui/material"
+import { ThemeProvider } from '@emotion/react'
+
+
+
+const App = () => {
+  return (
+   <ThemeProvider>
+    <h1>Tymes</h1>
+    <Button variant="contained"> App my buen Ñiero </Button>
+
+   </ThemeProvider>
+    
+   
+    
+     
+    
+  )
+}
+
+export default App
+
