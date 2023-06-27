62004c65c2a0db0ebe5b2be7b02789d95427cc4c
franSlepoy <slepoyfrancisco@gmail.com>
2023-06-27 20:33:26 -0300

    iconos en navDrawer

--- src/components/common/navbar/NavListDrawer.jsx
+++ src/components/common/navbar/NavListDrawer.jsx
@@ -6,8 +6,10 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
+import EmailTwoToneIcon from '@mui/icons-material/EmailTwoTone';
+import FacebookTwoToneIcon from '@mui/icons-material/FacebookTwoTone';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 export default function BasicList() {
   return (
@@ -15,11 +17,11 @@ export default function BasicList() {
       <nav aria-label="main mailbox folders">
         <List>
           <ListItem disablePadding>
-            <ListItemButton>
+            <ListItemButton component="a" href="#simple-list" >
               <ListItemIcon>
-                <DraftsIcon />
+              <EmailTwoToneIcon sx={{ fontSize: 33}} />
               </ListItemIcon>
-              <ListItemText primary="Email" />
+              <ListItemText primary="Email"/>
             </ListItemButton>
           </ListItem>
         </List>
@@ -28,13 +30,21 @@ export default function BasicList() {
       <nav aria-label="secondary mailbox folders">
         <List>
           <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemText primary="Trash" />
+            <ListItemButton component="a" href="#simple-list">
+              <InstagramIcon sx={{mr: 3, fontSize:33}}/>
+              <ListItemText primary="Instagram" />
             </ListItemButton>
           </ListItem>
           <ListItem disablePadding>
             <ListItemButton component="a" href="#simple-list">
-              <ListItemText primary="Spam" />
+            <FacebookTwoToneIcon sx={{mr: 3, fontSize:33}} />
+              <ListItemText primary="Facebook"/>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+            <YouTubeIcon sx={{mr: 3, fontSize:33}} />
+              <ListItemText primary="YouTube"/>
             </ListItemButton>
           </ListItem>
         </List>
